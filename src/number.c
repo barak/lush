26a7e5d2e4f439eda60e0a4a8781e9f29df59767
rjuengling <rjuengling@b68d3702-14d2-4b18-b47c-39ed2627325e>
2008-11-10 01:36:52 +0000

    added missing src/number.c (followup on rev 144)
    git-svn-id: https://lush.svn.sourceforge.net/svnroot/lush/lush2-branches/lush2-perf@147 b68d3702-14d2-4b18-b47c-39ed2627325e

---
+++ src/number.c
@@ -0,0 +1,60 @@
+
+#include "header.h"
+
+bool integerp(double d)
+{
+   return d == nearbyint(d);
+}
+
+DX(xintegerp)
+{
+   ARG_NUMBER(1);
+   ARG_EVAL(1);
+   return NEW_BOOL(integerp(AREAL(1)));
+}
+
+
+bool oddp(double d)
+{
+   ifn (integerp(d))
+      RAISEF("not an integer", NEW_NUMBER(d));
+   return d/2.0 != nearbyint(d/2.0);
+}
+
+DX(xoddp)
+{
+   ARG_NUMBER(1);
+   ARG_EVAL(1);
+   return NEW_BOOL(oddp(AREAL(1)));
+}
+
+
+bool evenp(double d)
+{
+   ifn (integerp(d))
+      RAISEF("not an integer", NEW_NUMBER(d));
+   return d/2.0 == nearbyint(d/2.0);
+}
+
+DX(xevenp)
+{
+   ARG_NUMBER(1);
+   ARG_EVAL(1);
+   return NEW_BOOL(evenp(AREAL(1)));
+}
+
+
+void init_number(void)
+{
+   dx_define("integerp", xintegerp);
+   dx_define("oddp", xoddp);
+   dx_define("evenp", xevenp);
+}
+
+
+/* -------------------------------------------------------------
+   Local Variables:
+   c-file-style: "k&r"
+   c-basic-offset: 3
+   End:
+   ------------------------------------------------------------- */
